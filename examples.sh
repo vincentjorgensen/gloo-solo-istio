1fe5b489ad0ee5436fabd15c70eb32a050026859
Vincent Jorgensen <vincent.jorgensen@solo.io>
2025-06-06 15:42:06 -0700

    moving deploy to new file

---
+++ examples.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$0")"/functions.sh
+
+istio_ambient_with_kgateway_and_eastwest() {
+  local _cluster1 _cluster2 _istio
+  _cluster1=$1
+  _cluster2=$2
+  _istio=1.26
+
+  deploy_istio_ambient "$_cluster1" "$_istio"
+  install_kgateway_crds "$_cluster1"
+  deploy_kgateway_eastwest "$_cluster1"
+
+  deploy_istio_ambient "$_cluster2" "$_istio"
+  install_kgateway_crds "$_cluster2"
+  deploy_kgateway_eastwest "$_cluster2"
+
+  install_kgateway_ew_link "$_cluster1" "$_cluster1" "$_cluster1" "$_cluster2"
+  install_kgateway_ew_link "$_cluster2" "$_cluster2" "$_cluster2" "$_cluster1"
+
+  install_helloworld_app -x "$_cluster1" -a
+  install_helloworld_app -x "$_cluster2" -a
+
+  create_namespace "$_cluster1" "$_cluster1" ingress-gateways
+  install_kgateway_ingress_gateway "$_context" ingress-gateway istio-gateways '*.example.com' 80
+  install_kgateway_httproute "$_context" ingress-gateway istio-gateways helloworld helloworld 8001 helloworld.example.com
+  install_kgateway_reference_grant "$_context" 
+}
