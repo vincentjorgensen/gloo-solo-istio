c7119bec13e27b1321318bbaa10432e6cdafd691
Vincent Jorgensen <vincent.jorgensen@solo.io>
2025-08-01 13:39:46 -0700

    Convert to enable plays

---
+++ infras/infra_aws_mc_cert_manager.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+export GSI_CLUSTER=c1-us-west-2
+export GSI_CONTEXT=c1-us-west-2
+export GSI_NETWORK=c1-us-west-2
+
+export GSI_REMOTE_CLUSTER=c2-us-west-2
+export GSI_REMOTE_CONTEXT=c2-us-west-2
+export GSI_REMOTE_NETWORK=c2-us-west-2
+
+export DOCKER_DESKTOP_ENABLED=false
+export AWS_ENABLED=true
+
+export AMBIENT_ENABLED=true
+export MULTICLUSTER_ENABLED=true
+export CERT_MANAGER_ENABLED=true
+
+export GLOO_GATEWAY_V2_ENABLED=true
+
+export HELLOWORLD_ENABLED=true
+export CURL_ENABLED=true
+export TOOLS_ENABLED=true
+export HTTPBIN_ENABLED=true
---
+++ infras/infra_gme_mc_ambient.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+export GSI_CLUSTER=cluster1
+export GSI_CONTEXT=cluster1
+export GSI_NETWORK=cluster1
+
+export GSI_REMOTE_CLUSTER=cluster2
+export GSI_REMOTE_CONTEXT=cluster2
+export GSI_REMOTE_NETWORK=cluster2
+
+export GME_MGMT_CLUSTER=mgmt
+export GME_MGMT_CONTEXT=mgmt
+
+export AMBIENT_ENABLED=true
+export CURL_ENABLED=true
+export EXTERNAL_DNS_ENABLED=true
+export GLOO_GATEWAY_V2_ENABLED=true
+export GME_ENABLED=true
+export HELLOWORLD_ENABLED=true
+export MULTICLUSTER_ENABLED=true
+export TOOLS_ENABLED=true
