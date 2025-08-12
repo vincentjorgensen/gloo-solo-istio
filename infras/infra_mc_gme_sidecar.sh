31ed28c1a8c2a251205121f07681c8e1719639ab
Vincent Jorgensen <vincent.jorgensen@solo.io>
2025-08-12 14:04:16 -0700

    more infras

---
+++ infras/infra_mc_gme_sidecar.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+export GSI_ARGOCD_CLUSTER=cluster2
+export GSI_ARGOCD_CONTEXT=cluster2
+export GSI_ARGOCD_NETWORK=cluster2
+
+export GSI_MGMT_CLUSTER=mgmt
+export GSI_MGMT_CONTEXT=mgmt
+export GSI_MGMT_NETWORK=mgmt
+
+export GSI_CLUSTER=cluster1
+export GSI_CONTEXT=cluster1
+export GSI_NETWORK=cluster1
+
+export GSI_REMOTE_CLUSTER=cluster2
+export GSI_REMOTE_CONTEXT=cluster2
+export GSI_REMOTE_NETWORK=cluster2
+
+export SIDECAR_ENABLED=true
+export MULTICLUSTER_ENABLED=true
+
+export GME_ENABLED=true
+export ISTIO_GATEWAY_ENABLED=true
+
+export HELLOWORLD_ENABLED=true
+export CURL_ENABLED=true
+export UTILS_ENABLED=true
+export NETSHOOT_ENABLED=true
+export HTTPBIN_ENABLED=true
+
+export EXTERNAL_DNS_ENABLED=true
