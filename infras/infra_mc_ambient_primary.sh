ac94dcbc117b6362535bcd0a42ed6a8ed3ac9220
Vincent Jorgensen <vincent.jorgensen@solo.io>
2025-08-21 16:13:24 -0700

    other mc use cases

---
+++ infras/infra_mc_ambient_primary.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+export GSI_CLUSTER=cluster1
+export GSI_CONTEXT=cluster1
+export GSI_NETWORK=net-cluster1
+
+export GSI_REMOTE_CLUSTER=cluster2
+export GSI_REMOTE_CONTEXT=cluster2
+export GSI_REMOTE_NETWORK=net-cluster2
+
+export AMBIENT_ENABLED=true
+export MULTICLUSTER_ENABLED=true
