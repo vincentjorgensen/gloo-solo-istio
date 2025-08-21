747abc4a9d72b9f77742e88e77df091c6f685a21
Vincent Jorgensen <vincent.jorgensen@solo.io>
2025-08-20 17:38:58 -0700

    adding aws for corelight

--- infras/infra_aws_corelight.sh
+++ infras/infra_aws_corelight.sh
@@ -4,14 +4,20 @@ export GSI_CONTEXT=c1-us-west-2
 export GSI_NETWORK=c1-us-west-2
 export AWS_REGION=us-west-2
 
+export GSI_MGMT_CLUSTER=c1-us-west-2
+export GSI_MGMT_CONTEXT=c1-us-west-2
+
 export DOCKER_DESKTOP_ENABLED=false
 export AWS_ENABLED=true
 
 export GLOO_GATEWAY_V1_ENABLED=true
+export GLOO_GATEWAY_V1_VER=1.19.4
+export GATEWAY_API_ENABLED=true     # V1 but with Gateway API
 export HTTPS_ENABLED=true
 export INGRESS_SIZE=3
 
 export GME_ENABLED=true
+export GME_VER=2.9.2
 
 export TLDN=soloio.vincentjorgensen.com
 
